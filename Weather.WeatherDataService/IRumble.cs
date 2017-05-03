1f92266ce91e2b8b5c4e67b851a306524a6a4e93
atharmar <Aruna.Tharmar@bcbsnc.com>
2017-05-03 17:16:14 -0400

    Added IRumble

---
+++ Weather.WeatherDataService/IRumble.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public interface IRumble
+    {
+    DateTime EventTime { get; }
+    decimal Latitude { get; }
+    decimal Longitude { get; }
+    decimal Ddepth  { get; }
+    decimal Magnitude  { get; }
+    string MagnitudeType { get; }
+    int SeismicStations { get; }
+    decimal AzimuthalGap  { get; }
+    decimal EpiCenterDistance { get; }
+    decimal RootMeanSquare { get; }
+    string DataContributorId { get; }
+    string NetworkIdentifie { get; }
+    DateTime RecentUpdateTime { get; }
+    string GeographicRegion { get; }
+    string SeismicEventType { get; }
+    decimal HorizontalError { get; }
+    decimal DepthError { get; }
+    decimal MagnitudeError { get; }
+    int MagniteOfEarthQuake { get; }
+    string EventsReviewed { get; }
+    string LocationSource { get; }
+    string MagnitudeSource { get; }
+}
