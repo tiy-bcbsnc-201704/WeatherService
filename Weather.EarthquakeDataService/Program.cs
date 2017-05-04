b3d60cc501a9a73dccd45c37dcd77d93bd2d074c
atharmar <Aruna.Tharmar@bcbsnc.com>
2017-05-04 09:02:32 -0400

    Fixed field name typos

--- Weather.EarthquakeDataService/Program.cs
+++ Weather.EarthquakeDataService/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Weather.EarthquakeDataService
+namespace Weather.WeatherDataService
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    WebClient wc = new WebClient();
+                    wc.DownloadFile("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.csv", @"C:\Users\u224764\Downloads\all_hour.csv");
+                }
+                catch (WebException)
+                {
+                    Console.WriteLine("File download is not successful. Try again!!");
+                }
+            }
+
         }
     }
 }
