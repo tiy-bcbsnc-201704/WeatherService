f0b460f3e59ff74bd3c5bf04cf5f88a36f0f6b25
Patrick Burkart <Pat.Burkart@bcbsnc.com>
2017-05-04 10:38:37 -0400

    v0.0

--- Weather.LocationTranslator/LongLatCalcs.cs
+++ Weather.LocationTranslator/LongLatCalcs.cs
@@ -8,12 +8,33 @@ namespace Weather.LocationTranslator
         public string StateForLongLat(double longitude, double latitude)
         {
 
-            string longLatState = "AK";
+            string longLatState = "";
+            double plusOrminus = 0;
+            double maxplusOrminus = 5.0;
 
 
             // determine longLatState
+            /*
+            while (true)
+                {
+                longLatState = select count(*)
+                    if (any found)
+                    {
+                    return longLatState;
+                }
+                    else if (plusOrminus == maxplusOrminus)
+                {
+                    return "XX";
+                }
+                else
+                {
+                    plusORminus += .10;
 
+                }
+            }
+            */
 
+            longLatState = "AK";
             return longLatState;
 
         }
