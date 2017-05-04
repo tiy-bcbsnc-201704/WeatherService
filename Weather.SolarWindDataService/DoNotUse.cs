28af2c58ff9242897d55f5caece1a44714f7c7c3
Bryan Rosenberg <Bryan.Rosenberg@bcbsnc.com>
2017-05-04 12:18:29 -0400

    Working version reading/writing ... still dupes

---
+++ Weather.SolarWindDataService/DoNotUse.cs
@@ -0,0 +1,74 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+
+namespace Weather.SolarWindDataService
+{
+    class DoNotUse
+    {
+        private DateTime _measurementDateTime;
+        private decimal _xCoordinate;
+        private decimal _yCoordinate;
+        private decimal _zCoordinate;
+        private decimal _longitude;
+        private decimal _latitude;
+        private decimal _temperature;
+
+        public DoNotUse() { }
+
+        public DoNotUse(DateTime MeasurementDateTime, decimal XCoordinate, decimal YCoordinate, decimal ZCoordinate, decimal Longitude, decimal Latitude, decimal Temperature)
+        {
+            _measurementDateTime = MeasurementDateTime;
+            _xCoordinate = XCoordinate;
+            _yCoordinate = YCoordinate;
+            _zCoordinate = ZCoordinate;
+            _longitude = Longitude;
+            _latitude = Latitude;
+            _temperature = Temperature;
+        }
+
+        [Key]
+        public int SolarWindId { get; set; }
+
+        public DateTime MeasurementDateTime
+        {
+            get { return _measurementDateTime; }
+            set { _measurementDateTime = value; }
+        }
+
+        public decimal XCoordinate
+        {
+            get { return _xCoordinate; }
+            set { _xCoordinate = value; }
+        }
+
+        public decimal YCoordinate
+        {
+            get { return _yCoordinate; }
+            set { _yCoordinate = value; }
+        }
+
+        public decimal ZCoordinate
+        {
+            get { return _zCoordinate; }
+            set { _zCoordinate = value; }
+        }
+
+        public decimal Longitude
+        {
+            get { return _longitude; }
+            set { _longitude = value; }
+        }
+
+        public decimal Latitude
+        {
+            get { return _latitude; }
+            set { _latitude = value; }
+        }
+
+        public decimal Temperature
+        {
+            get { return _temperature; }
+            set { _temperature = value; }
+        }
+    }
+}
--- Weather.SolarWindDataService/SolarWind.cs
+++ Weather.SolarWindDataService/SolarWind.cs
@@ -3,72 +3,17 @@ using System;
 
 namespace Weather.SolarWindDataService
 {
-    class SolarWind
+    [Table("SolarWind")]
+    public class SolarWind
     {
-        private DateTime _measurementDateTime;
-        private decimal _xCoordinate;
-        private decimal _yCoordinate;
-        private decimal _zCoordinate;
-        private decimal _longitude;
-        private decimal _latitude;
-        private decimal _temperature;
-
-        public SolarWind() { }
-
-        public SolarWind(DateTime MeasurementDateTime, decimal XCoordinate, decimal YCoordinate, decimal ZCoordinate, decimal Longitude, decimal Latitude, decimal Temperature)
-        {
-            _measurementDateTime = MeasurementDateTime;
-            _xCoordinate = XCoordinate;
-            _yCoordinate = YCoordinate;
-            _zCoordinate = ZCoordinate;
-            _longitude = Longitude;
-            _latitude = Latitude;
-            _temperature = Temperature;
-        }
-
         [Key]
         public int SolarWindId { get; set; }
-
-        public DateTime MeasurementDateTime
-        {
-            get { return _measurementDateTime; }
-            set { _measurementDateTime = value; }
-        }
-
-        public decimal XCoordinate
-        {
-            get { return _xCoordinate; }
-            set { _xCoordinate = value; }
-        }
-
-        public decimal YCoordinate
-        {
-            get { return _yCoordinate; }
-            set { _yCoordinate = value; }
-        }
-
-        public decimal ZCoordinate
-        {
-            get { return _zCoordinate; }
-            set { _zCoordinate = value; }
-        }
-
-        public decimal Longitude
-        {
-            get { return _longitude; }
-            set { _longitude = value; }
-        }
-
-        public decimal Latitude
-        {
-            get { return _latitude; }
-            set { _latitude = value; }
-        }
-
-        public decimal Temperature
-        {
-            get { return _temperature; }
-            set { _temperature = value; }
-        }
+        public DateTime MeasurementDateTime { get; set; }
+        public decimal XCoordinate { get; set; }
+        public decimal YCoordinate { get; set; }
+        public decimal ZCoordinate { get; set; }
+        public decimal Longitude { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Temperature { get; set; }
     }
 }
