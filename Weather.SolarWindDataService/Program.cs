1e4d88a5d4763f98a02f752f94bc32479a47483d
Bryan Rosenberg <Bryan.Rosenberg@bcbsnc.com>
2017-05-04 12:26:54 -0400

    Added 60 second delay

--- Weather.SolarWindDataService/Program.cs
+++ Weather.SolarWindDataService/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.IO;
 using System.Net;
+using System.Threading;
 
 namespace Weather.SolarWindDataService
 {
@@ -14,53 +15,57 @@ namespace Weather.SolarWindDataService
         {
 
             // Download the file into a string
-            using (WebClient webClient = new WebClient())
+            while (true)
             {
-                //webClient.DownloadFile("http://services.swpc.noaa.gov/products/solar-wind/mag-5-minute.json", @"C:\Users\u872059\Downloads\SolarWind.json");
-                string solarWindData = webClient.DownloadString("http://services.swpc.noaa.gov/products/solar-wind/mag-5-minute.json");
-                Console.WriteLine(solarWindData);
-
-                // Parse the JSON file data
-                JArray solarWindDataArray = JArray.Parse(solarWindData);
-                
-                //Console.WriteLine(a[0]);
-                Console.WriteLine(solarWindDataArray[1]);
-                Console.WriteLine(solarWindDataArray[2]);
-
-                for (int i = 1; i < 3; i += 1)
+                Thread.Sleep(60000); // 60 seconds
+                using (WebClient webClient = new WebClient())
                 {
-                    List<object> windData = new List<object>(solarWindDataArray[i]);
+                    //webClient.DownloadFile("http://services.swpc.noaa.gov/products/solar-wind/mag-5-minute.json", @"C:\Users\u872059\Downloads\SolarWind.json");
+                    string solarWindData = webClient.DownloadString("http://services.swpc.noaa.gov/products/solar-wind/mag-5-minute.json");
+                    //Console.WriteLine(solarWindData);
 
-                    //Console.WriteLine("------------------------------------");
-                    //Console.WriteLine(windData[0]);
-                    //Console.WriteLine(windData[1]);
-                    //Console.WriteLine(windData[2]);
-                    //Console.WriteLine(windData[3]);
-                    //Console.WriteLine(windData[4]);
-                    //Console.WriteLine(windData[5]);
-                    //Console.WriteLine(windData[6]);
-                    //Console.WriteLine("------------------------------------");
-                    //Console.ReadLine();
+                    // Parse the JSON file data
+                    JArray solarWindDataArray = JArray.Parse(solarWindData);
 
-                    string connectionString = "Server=LOCALHOST;Database=WeatherService;Trusted_Connection=True";
-                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    //Console.WriteLine(a[0]);
+                    //Console.WriteLine(solarWindDataArray[1]);
+                    //Console.WriteLine(solarWindDataArray[2]);
+
+                    for (int i = 1; i < 3; i += 1)
                     {
-                        SolarWind solarWind = new SolarWind();
+                        List<object> windData = new List<object>(solarWindDataArray[i]);
 
-                        solarWind.MeasurementDateTime = Convert.ToDateTime(windData[0].ToString());
-                        solarWind.XCoordinate = Convert.ToDecimal(windData[1].ToString());
-                        solarWind.YCoordinate = Convert.ToDecimal(windData[2].ToString());
-                        solarWind.ZCoordinate = Convert.ToDecimal(windData[3].ToString());
-                        solarWind.Longitude = Convert.ToDecimal(windData[4].ToString());
-                        solarWind.Latitude = Convert.ToDecimal(windData[5].ToString());
-                        solarWind.Temperature = Convert.ToDecimal(windData[6].ToString());
+                        //Console.WriteLine("------------------------------------");
+                        //Console.WriteLine(windData[0]);
+                        //Console.WriteLine(windData[1]);
+                        //Console.WriteLine(windData[2]);
+                        //Console.WriteLine(windData[3]);
+                        //Console.WriteLine(windData[4]);
+                        //Console.WriteLine(windData[5]);
+                        //Console.WriteLine(windData[6]);
+                        //Console.WriteLine("------------------------------------");
+                        //Console.ReadLine();
 
-                        connection.Insert(solarWind);
+                        string connectionString = "Server=LOCALHOST;Database=WeatherService;Trusted_Connection=True";
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            SolarWind solarWind = new SolarWind();
+
+                            solarWind.MeasurementDateTime = Convert.ToDateTime(windData[0].ToString());
+                            solarWind.XCoordinate = Convert.ToDecimal(windData[1].ToString());
+                            solarWind.YCoordinate = Convert.ToDecimal(windData[2].ToString());
+                            solarWind.ZCoordinate = Convert.ToDecimal(windData[3].ToString());
+                            solarWind.Longitude = Convert.ToDecimal(windData[4].ToString());
+                            solarWind.Latitude = Convert.ToDecimal(windData[5].ToString());
+                            solarWind.Temperature = Convert.ToDecimal(windData[6].ToString());
+
+                            connection.Insert(solarWind);
+                        }
                     }
                 }
-            }
 
-            // Call the EventNotifier service to record the time the file was downloaded
+                // Call the EventNotifier service to record the time the file was downloaded
+            }
         }
     }
 }
