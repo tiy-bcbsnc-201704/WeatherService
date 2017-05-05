b9e8ca9c642984d103952efe8a7aee926a2e7b9a
Patrick Burkart <Pat.Burkart@bcbsnc.com>
2017-05-05 16:01:23 -0400

    v1.3 - Geographic Center

---
+++ Weather.LocationTranslator/GeographicCenter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.LocationTranslator
+{
+    public class GeographicCenter
+    {
+        public decimal CenterLatitude { get; set; }
+
+        public decimal CenterLongitude { get; set; }
+
+    }
+}
--- Weather.LocationTranslator/LongLatCalcs.cs
+++ Weather.LocationTranslator/LongLatCalcs.cs
@@ -14,7 +14,7 @@ namespace Weather.LocationTranslator
         {
             _connectionString = connectionString;
         }
-    
+
         public GeographicBounds BoundsForState(StateCode code)
         {
             using (SqlConnection connection = new SqlConnection(_connectionString))
@@ -31,79 +31,93 @@ namespace Weather.LocationTranslator
         }
 
 
-        public StateCode StateForLongLat(double longitude, double latitude)
-    {
-
-
-        string longlatState = "";
-        double plusOrminus = 0;
-
-        // !! search control !! 
-        double incrplusOrminus = 0.1;
-        double maxplusOrminus = 5.0;
-
-
-        while (true)
+        public GeographicCenter CenterOfState(StateCode code)
         {
-
-            longlatState = CheckCoords(longitude, latitude, plusOrminus);
-            // Console.WriteLine($"Call: {longitude},{latitude},{plusOrminus} got {longlatState}");
-
-            if (longlatState != "ZZ")
+            using (SqlConnection connection = new SqlConnection(_connectionString))
             {
-                // return longlatState;
-                return (StateCode)Enum.Parse(typeof(StateCode), longlatState);
+                return connection.QuerySingle<GeographicCenter>(@"
+                    SELECT (SUM(Latitude) / COUNT(StateCode)) AS CenterLatitude
+                           , (SUM(Longitude) / COUNT(StateCode)) AS CenterLongitude
+                      FROM LongLatLocs
+                     WHERE StateCode = @Code 
+                ", new { Code = code.ToString() });
             }
-            if (plusOrminus >= maxplusOrminus)
-            {
-                // return "XX";   
-                return (StateCode)Enum.Parse(typeof(StateCode), "XX");
-            }
-            plusOrminus += incrplusOrminus;
         }
 
 
-    }
-
-
-
-
-    public string CheckCoords(double longitude, double latitude, double plusorminus)
-    {
-
-        //string _connectionString = "Server=localhost;Database=WeatherService;Integrated Security=True";
-
-        using (SqlConnection connection = new SqlConnection(_connectionString))
+        public StateCode StateForLongLat(double longitude, double latitude)
         {
 
-            connection.Open();
 
-            SqlCommand command = connection.CreateCommand();
-            command.CommandText = @"
+            string longlatState = "";
+            double plusOrminus = 0;
+
+            // !! search control !! 
+            double incrplusOrminus = 0.1;
+            double maxplusOrminus = 5.0;
+
+
+            while (true)
+            {
+
+                longlatState = CheckCoords(longitude, latitude, plusOrminus);
+                // Console.WriteLine($"Call: {longitude},{latitude},{plusOrminus} got {longlatState}");
+
+                if (longlatState != "ZZ")
+                {
+                    // return longlatState;
+                    return (StateCode)Enum.Parse(typeof(StateCode), longlatState);
+                }
+                if (plusOrminus >= maxplusOrminus)
+                {
+                    // return "XX";   
+                    return (StateCode)Enum.Parse(typeof(StateCode), "XX");
+                }
+                plusOrminus += incrplusOrminus;
+            }
+
+
+        }
+
+
+
+
+        public string CheckCoords(double longitude, double latitude, double plusorminus)
+        {
+
+            //string _connectionString = "Server=localhost;Database=WeatherService;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+
+                connection.Open();
+
+                SqlCommand command = connection.CreateCommand();
+                command.CommandText = @"
                     Select StateCode, COUNT(*) From LongLatLocs Where
                            (Longitude >= @longitude - @plusorminus and Longitude <= @longitude + @plusorminus) and
                            (Latitude  >= @latitude  - @plusorminus and Latitude  <= @latitude  + @plusorminus) 
                            Group By StateCode Order By 2 Desc";
 
-            command.Parameters.AddWithValue("@longitude", longitude);
-            command.Parameters.AddWithValue("@latitude", latitude);
-            command.Parameters.AddWithValue("@plusorminus", plusorminus);
-            SqlDataReader reader = command.ExecuteReader();
+                command.Parameters.AddWithValue("@longitude", longitude);
+                command.Parameters.AddWithValue("@latitude", latitude);
+                command.Parameters.AddWithValue("@plusorminus", plusorminus);
+                SqlDataReader reader = command.ExecuteReader();
 
-            if (reader.HasRows == false)
-            {
-                return "ZZ";
+                if (reader.HasRows == false)
+                {
+                    return "ZZ";
+                }
+
+                reader.Read();
+                return reader.GetValue(0).ToString();
+
             }
-
-            reader.Read();
-            return reader.GetValue(0).ToString();
-
         }
+
+
+
+
     }
-
-
-
-
-}
 }
 
